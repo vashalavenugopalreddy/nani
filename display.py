96ca0f2eda921a654708547711566349e414ef18
vashala venu gopal reddy / data scientist <115161949+vashalavenugopalreddy@users.noreply.github.com>
2022-11-20 11:55:23 +0530

    Update display.py

--- display.py
+++ display.py
@@ -2,7 +2,7 @@ list1=[10, 20, 30, 40, 50]
 rest_list1=[x*2 for x in list1]
 print(rest_list1)
             
-list2-[100, 200, 300, 400, 500]
+list2=[100, 200, 300, 400, 500]
 rest_list2=[x*0.1 for x in list2]
 print(rest_list2)
 print("the list is completed")
